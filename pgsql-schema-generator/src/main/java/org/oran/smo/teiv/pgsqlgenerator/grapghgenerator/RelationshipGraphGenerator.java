5ca9ddd44aad55e27ec94fc3649138c3a1f98445
JvD_Ericsson <jeff.van.dam@est.tech>
2024-07-04 14:36:27 +0100

    Auto gen entity graph
    Generate entity and object graphs in the pgsql-schema-generator
    from the entites defined in the provided yang models
    
    Output location and option to generate the files in application.yaml
    
    Data types are DB datatypes not YANG
    
    Issue-ID: SMO-156
    Change-Id: Id529c2c2cfdb3af1d7f53394e3e09ef93389d968
    Signed-off-by: JvD_Ericsson <jeff.van.dam@est.tech>

--- pgsql-schema-generator/src/main/java/org/oran/smo/teiv/pgsqlgenerator/grapghgenerator/RelationshipGraphGenerator.java
+++ pgsql-schema-generator/src/main/java/org/oran/smo/teiv/pgsqlgenerator/grapghgenerator/RelationshipGraphGenerator.java
@@ -18,7 +18,7 @@
  *  SPDX-License-Identifier: Apache-2.0
  *  ============LICENSE_END=========================================================
  */
-package org.oran.smo.teiv.pgsqlgenerator.relationshipgrapghgenerator;
+package org.oran.smo.teiv.pgsqlgenerator.grapghgenerator;
 
 import guru.nidi.graphviz.model.Factory;
 import guru.nidi.graphviz.model.MutableGraph;
@@ -40,10 +40,10 @@ import java.util.List;
 @Slf4j
 public class RelationshipGraphGenerator {
 
-    @Value("${relationship-graph.generate}")
+    @Value("${graphs.generate}")
     private boolean generateRelationshipGraph;
 
-    @Value("${relationship-graph.output}")
+    @Value("${graphs.output}")
     private String graphOutput;
 
     public void generate(List<Relationship> relationships, List<Entity> entities) {
@@ -59,17 +59,18 @@ public class RelationshipGraphGenerator {
             }
             generateGraph("overall", relationships, entities);
         } else {
-            log.info("generate-relationship-graph set to false");
+            log.info("graphs.generate set to false");
         }
     }
 
     private void generateGraph(String name, List<Relationship> relationships, List<Entity> entities) {
+        String filename = name + "-rel";
         MutableGraph g = prepareGraph(relationships, entities);
-        File outputFile = new File(graphOutput, name);
+        File outputFile = new File(graphOutput, filename);
         try {
             Graphviz.fromGraph(g).render(Format.SVG).toFile(outputFile);
         } catch (IOException e) {
-            log.error("Error when rendering {} graph to svg", name);
+            log.error("Error when rendering {} graph to svg", filename);
             throw new RuntimeException(e);
         }
         System.out.println("Graph rendered to: " + outputFile.getAbsolutePath());
