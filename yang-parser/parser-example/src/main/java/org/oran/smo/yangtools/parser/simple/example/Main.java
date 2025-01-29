a305498065afe053a7098341cdf5e0c72a529cf6
JvD_Ericsson <jeff.van.dam@est.tech>
2025-01-29 16:09:48 +0000

    add parser-example application
    ISsue-ID: SMO-
    Change-Id: I22623b86878735b1436c8a39e04658d389bbac9a
    Signed-off-by: JvD_Ericsson <jeff.van.dam@est.tech>

---
+++ yang-parser/parser-example/src/main/java/org/oran/smo/yangtools/parser/simple/example/Main.java
@@ -0,0 +1,130 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Ericsson
+ *  Modifications Copyright (C) 2024 OpenInfra Foundation Europe
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+package org.oran.smo.yangtools.parser.simple.example;
+
+import org.oran.smo.yangtools.parser.ParserExecutionContext;
+import org.oran.smo.yangtools.parser.YangDeviceModel;
+import org.oran.smo.yangtools.parser.data.parser.JsonParser.JsonArray;
+import org.oran.smo.yangtools.parser.data.parser.JsonParser.JsonObject;
+import org.oran.smo.yangtools.parser.data.parser.JsonParser.JsonObjectMemberName;
+import org.oran.smo.yangtools.parser.data.parser.JsonParser.JsonValue;
+import org.oran.smo.yangtools.parser.data.parser.JsonWriter;
+import org.oran.smo.yangtools.parser.findings.FindingsManager;
+import org.oran.smo.yangtools.parser.findings.ModifyableFindingSeverityCalculator;
+import org.oran.smo.yangtools.parser.findings.ModuleAndFindingTypeAndSchemaNodePathFilterPredicate;
+import org.oran.smo.yangtools.parser.model.YangModel;
+import org.oran.smo.yangtools.parser.model.statements.ietf.IetfExtensionsClassSupplier;
+import org.oran.smo.yangtools.parser.model.statements.oran.OranExtensionsClassSupplier;
+import org.oran.smo.yangtools.parser.model.statements.threegpp.ThreeGppExtensionsClassSupplier;
+import org.oran.smo.yangtools.parser.model.statements.yang.YModule;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class Main {
+
+    private static YangDeviceModel yangDeviceModel;
+    private static ModifyableFindingSeverityCalculator severityCalculator;
+    private static FindingsManager findingsManager;
+    private static ParserExecutionContext context;
+
+    public static void main(String[] args) {
+
+        // Setup for yang parser.
+        // The parser execution context directs the behaviour of the parser. It is vital that the
+        // context is correctly setup.
+        setUp();
+
+        // using the yang-parser's jsonParser parse json output from TEIV response:
+        // GET http://localhost:31074/topology-inventory/v1alpha11/domains/EQUIPMENT/entity-types/AntennaModule/entities?targetFilter=/attributes
+        JsonObject parsedJsonObject = (JsonObject) JsonParserExample.parseJsonFile(context);
+        // get the "items" from the response
+        JsonArray items = (JsonArray) getJsonObjectMemberValue(parsedJsonObject, "items");
+        System.out.println("Response of all AntennaModules from TEIV:");
+        for (JsonValue item : items.getValues()) {
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try {
+                JsonValue value = getJsonObjectMemberValue((JsonObject) item, "o-ran-smo-teiv-equipment:AntennaModule");
+                // using the yang-parser's jsonWriter write the json to stream
+                JsonWriter.write(value, baos);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            // print the output to console
+            System.out.println(baos);
+        }
+
+        System.out.println("\n------------------------------------------------------------------");
+
+        // using the yang-parser's parseIntoYangModels
+        List<YangModel> yangModels = YangParserExample.parseYangModels(yangDeviceModel, context);
+        System.out.println("\nList of all entities/lists from o-ran-smo-teiv-ran model:");
+        Optional<YangModel> optionalRanModel = yangModels.stream().filter(yangModel -> "o-ran-smo-teiv-ran".equals(yangModel
+                .getModuleIdentity().getModuleName())).findFirst();
+        optionalRanModel.ifPresent(yangModel -> {
+            YModule yModule = yangModel.getYangModelRoot().getModule();
+            yModule.getLists().stream().forEach(yList -> {
+                System.out.println(yList.getListName());
+            });
+        });
+
+    }
+
+    private static JsonValue getJsonObjectMemberValue(final JsonObject jsonObject, final String memberName) {
+
+        final Set<Map.Entry<JsonObjectMemberName, JsonValue>> entrySet = jsonObject.getValuesByMember().entrySet();
+        for (final Map.Entry<JsonObjectMemberName, JsonValue> entry : entrySet) {
+
+            if (memberName.equals(entry.getKey().getMemberName())) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    private static void setUp() {
+        yangDeviceModel = new YangDeviceModel("Yang Parser JAR Test Device Model");
+
+        severityCalculator = new ModifyableFindingSeverityCalculator();
+
+        findingsManager = new FindingsManager(severityCalculator);
+
+        // suppress all findings from ietf and iana
+        findingsManager.addFilterPredicate(ModuleAndFindingTypeAndSchemaNodePathFilterPredicate.fromString(
+                "ietf*,iana*;*;*"));
+
+        final ThreeGppExtensionsClassSupplier threeGppStatementFactory = new ThreeGppExtensionsClassSupplier();
+        final IetfExtensionsClassSupplier ietfStatementFactory = new IetfExtensionsClassSupplier();
+        final OranExtensionsClassSupplier oranStatementFactory = new OranExtensionsClassSupplier();
+
+        context = new ParserExecutionContext(findingsManager, Arrays.asList(threeGppStatementFactory, oranStatementFactory,
+                ietfStatementFactory));
+        context.setFailFast(false);
+        context.setSuppressFindingsOnUnusedSchemaNodes(true);
+    }
+
+}
---
+++ yang-parser/parser-example/src/main/java/org/oran/smo/yangtools/parser/simple/example/YangParserExample.java
@@ -0,0 +1,64 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Ericsson
+ *  Modifications Copyright (C) 2024 OpenInfra Foundation Europe
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+package org.oran.smo.yangtools.parser.simple.example;
+
+import org.oran.smo.yangtools.parser.ParserExecutionContext;
+import org.oran.smo.yangtools.parser.YangDeviceModel;
+import org.oran.smo.yangtools.parser.input.FileBasedYangInput;
+import org.oran.smo.yangtools.parser.model.ConformanceType;
+import org.oran.smo.yangtools.parser.model.YangModel;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class YangParserExample {
+
+    private static final String DIR = "yang-parser-example/src/main/resources/yang/";
+
+    public static List<YangModel> parseYangModels(YangDeviceModel yangDeviceModel, ParserExecutionContext context) {
+
+        List<String> yangImplementFiles = new ArrayList<>();
+        List<String> yangImportFiles = new ArrayList<>();
+
+        // these yang files are the versions as of 2025-01-29
+        // for the current versions of the yang files check teiv/src/main/resources/models
+        yangImplementFiles.add("2024-10-08-o-ran-smo-teiv-ran.yang");
+        yangImplementFiles.add("2024-05-24-o-ran-smo-teiv-common-yang-extensions.yang");
+        yangImplementFiles.add("2024-10-04-o-ran-smo-teiv-common-yang-types.yang");
+
+        final List<YangModel> yangModels = new ArrayList<>();
+
+        for (final String relativeFilePath : yangImplementFiles) {
+            yangModels.add(new YangModel(new FileBasedYangInput(new File(DIR + relativeFilePath)),
+                    ConformanceType.IMPLEMENT));
+        }
+        for (final String relativeFilePath : yangImportFiles) {
+            yangModels.add(new YangModel(new FileBasedYangInput(new File(DIR + "import/" + relativeFilePath)),
+                    ConformanceType.IMPORT));
+        }
+
+        yangDeviceModel.parseIntoYangModels(context, yangModels);
+
+        return yangModels;
+    }
+
+}
