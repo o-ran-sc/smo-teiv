c72b363c332fbf17059c3fae2ea7e635cd71b933
aravind.est <aravindhan.a@est.tech>
2025-03-13 13:03:00 +0000

    Fix chartmuseum segmentation fault error
    CM version 0.16.1 causing segmentation fault.
    Upgrading to CM version 0.16.2 fixes this issue.
    
    Change-Id: I22ee7d3620ca0dea4936bed9b6d20b9afd7651b8
    Signed-off-by: aravind.est <aravindhan.a@est.tech>

--- charts/install-teiv.sh
+++ charts/install-teiv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ============LICENSE_START=======================================================
-# Copyright (C) 2024 OpenInfra Foundation Europe. All rights reserved.
+# Copyright (C) 2024-2025 OpenInfra Foundation Europe. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 #
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-CM_VERSION="v0.16.1"
+CM_VERSION="v0.16.2"
 CM_PORT="8879"
 HELM_LOCAL_REPO="$ROOT_DIR/chartstorage"
 
