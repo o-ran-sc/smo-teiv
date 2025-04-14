148816648dd6e20910131521c82b7ab0cbc396e8
JvD_Ericsson <jeff.van.dam@est.tech>
2025-04-14 12:24:22 +0100

    TEIV - Create adapter from NCMP to TEIV
    Issue-ID: SMO-187
    Change-Id: I385f9863d4d28ca5f316b8d7c1570b1c388cafbe
    Signed-off-by: JvD_Ericsson <jeff.van.dam@est.tech>

---
+++ adapters/ncmp-to-teiv-adapter/src/main/java/org/oran/smo/ncmp_to_teiv_adapter/models/AbstractFunction.java
@@ -0,0 +1,37 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Modifications Copyright (C) 2025 OpenInfra Foundation Europe
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+package org.oran.smo.ncmp_to_teiv_adapter.models;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractFunction {
+
+    public abstract Map<String, Object> createRelationshipWithManagedElement(String managedElementId);
+
+    public abstract Map<String, Object> addTeivEntitiesAndRelationships(Map<String, List<Object>> entityMap,
+            Map<String, List<Object>> relationshipMap);
+
+    public abstract String getTeivEntityType();
+
+    public abstract String getTeivRelationshipWithManagedElement();
+    
+    public abstract void createRelationshipWithSmo(Map<String, List<Object>> relationshipMap);
+}
